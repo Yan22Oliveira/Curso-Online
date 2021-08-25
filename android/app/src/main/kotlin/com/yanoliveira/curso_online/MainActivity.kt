988943ca52d8d32ccb3a06c5049babdd6147ec2a
Yan Oliveira <43455958+Yan22Oliveira@users.noreply.github.com>
2021-08-24 21:34:17 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/yanoliveira/curso_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yanoliveira.curso_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/helpers/parametros.dart
@@ -0,0 +1,14 @@
+//API's
+const api = "https://curso-online-77550-default-rtdb.firebaseio.com/";
+
+const api_user           = api+"user.json";
+const api_user_cadastrar = api+"/user/";
+const api_user_dados     = api+"user/";
+
+const api_cartas = api+"user/";
+
+const key = 'AIzaSyACcQ1QYVjlsWoFWRpzZ1XL1hvjGF00gsM';
+
+const login = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+
+const signUp = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
